fe607ac71fd9da7c428eebef508bba387d446e24
xiaohuasheng <775079852@qq.com>
2016-05-04 12:53:23 +0800

    加了viewpoint使之适应手机屏幕

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <html>
 <head>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1,target-densitydpi=medium-dpi">
 <!-- 样式控制 -->
 <script type="text/javascript" src="http://www.francescomalagrino.com/BootstrapPageGenerator/3/js/jquery-2.0.0.min.js"></script>
 <script type="text/javascript" src="http://www.francescomalagrino.com/BootstrapPageGenerator/3/js/jquery-ui"></script>
